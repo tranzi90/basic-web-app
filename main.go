2dea4bfdb116300498e3bb708b548a09199c492e
denis_levchenko <tranzi90@gmail.com>
2024-03-02 21:29:06 +0300

    log fatal

--- main.go
+++ main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func Home(writer http.ResponseWriter, request *http.Request) {
+func Home(writer http.ResponseWriter, _ *http.Request) {
 	_, err := fmt.Fprintf(writer, "WUBBA LUBBA DUBDUB")
 	if err != nil {
-		return
+		println(err)
 	}
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 
-	http.ListenAndServe(":8080", nil)
+	//err := http.ListenAndServe(":8080", nil)
+	//
+	//println(err.Error())
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
